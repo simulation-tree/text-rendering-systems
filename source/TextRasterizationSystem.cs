7bc795a79956bf45ebca1b0df7bd96f1da51aa8a
Phill <philldamaskin@gmail.com>
2024-09-25 19:14:26 -0400

    use shared vertex generator function in font

--- source/TextRasterizationSystem.cs
+++ source/TextRasterizationSystem.cs
@@ -151,7 +151,6 @@ namespace Rendering.Systems
         {
             uint textMeshEntity = input.entity;
             rint fontReference = input.request.fontReference;
-            Vector2 alignment = input.request.alignment;
             uint fontEntity = world.GetReference(textMeshEntity, fontReference);
             Font font = new(world, fontEntity);
             if (font.IsCompliant())
@@ -181,7 +180,7 @@ namespace Rendering.Systems
                 }
 
                 USpan<char> text = world.GetArray<char>(textMeshEntity);
-                GenerateTextMesh(ref operation, font, text, alignment);
+                GenerateTextMesh(ref operation, font, text);
 
                 //update proof components to fulfil the type argument
                 if (world.TryGetComponent(textMeshEntity, out IsTextMesh textMeshProof))
@@ -213,63 +212,30 @@ namespace Rendering.Systems
             }
         }
 
-        private void GenerateTextMesh(ref Operation operation, Font font, USpan<char> text, Vector2 alignment)
+        private unsafe void GenerateTextMesh(ref Operation operation, Font font, USpan<char> text)
         {
             Entity fontEntity = font.entity;
             uint glyphCount = fontEntity.GetArrayLength<FontGlyph>();
-            int pixelSize = 32;
+            uint pixelSize = 32;
 
             //todo: fault: what if the font changes? this system has no way of knowing when to update the atlases+meshes involved
             CompiledFont compiledFont = GetOrCompileFont(fontEntity, glyphCount, pixelSize);
 
-            uint lineHeight = font.LineHeight;
-            int penX = 0;
-            int penY = 0;
-            (int xMin, int xMax, int yMin, int yMax) bounds = compiledFont.face.Bounds; //todo: put bounds info on font entities
-            float descender = compiledFont.face.Descender;
-            float verticalSpan = bounds.yMax - bounds.yMin;
             using UnmanagedArray<MeshVertexPosition> positions = new(text.Length * 4);
             using UnmanagedArray<MeshVertexUV> uvs = new(text.Length * 4);
             using UnmanagedArray<uint> indices = new(text.Length * 6);
-            Vector2 maxPosition = default;
+            USpan<Vector3> vertices = new(positions.AsSpan().pointer, positions.Length);
+            Vector2 maxPosition = font.GenerateVertices(text, vertices, pixelSize);
+
+            uint vertexIndex = 0;
             for (uint i = 0; i < text.Length; i++)
             {
                 char c = text[i];
                 IsGlyph glyph = compiledFont.glyphs[c];
                 if (c == '\n')
                 {
-                    penX = 0;
-                    penY += (int)lineHeight;
                     continue;
                 }
-
-                (int x, int y) glyphOffset = glyph.offset;
-                (int x, int y) glyphAdvance = glyph.advance;
-                (int x, int y) glyphSize = glyph.size;
-                (int x, int y) glyphBearing = glyph.bearing;
-                float glyphWidth = glyphSize.x / pixelSize;
-                float glyphHeight = glyphSize.y / pixelSize;
-                Vector3 origin = new(penX + (glyphOffset.x / pixelSize), penY + (glyphOffset.y / pixelSize), 0);
-                origin.Y -= (glyphSize.y - glyphBearing.y) / pixelSize;
-                Vector3 size = new(glyphWidth, glyphHeight, 0);
-                origin /= 64f;
-                size /= 64f;
-                MeshVertexPosition first = origin;
-                MeshVertexPosition second = origin + new Vector3(size.X, 0, 0);
-                MeshVertexPosition third = origin + new Vector3(size.X, size.Y, 0);
-                MeshVertexPosition fourth = origin + new Vector3(0, size.Y, 0);
-                penX += glyphAdvance.x / pixelSize;
-                //penY += advance.y / pixelSize;
-
-                positions[(i * 4) + 0] = first;
-                positions[(i * 4) + 1] = second;
-                positions[(i * 4) + 2] = third;
-                positions[(i * 4) + 3] = fourth;
-
-                maxPosition = Vector2.Max(maxPosition, new(first.value.X, first.value.Y));
-                maxPosition = Vector2.Max(maxPosition, new(second.value.X, second.value.Y));
-                maxPosition = Vector2.Max(maxPosition, new(third.value.X, third.value.Y));
-                maxPosition = Vector2.Max(maxPosition, new(fourth.value.X, fourth.value.Y));
 
                 Vector4 region = compiledFont.regions[c];
                 MeshVertexUV firstUv = new(region.X, region.W);
@@ -281,11 +247,7 @@ namespace Rendering.Systems
                 uvs[(i * 4) + 1] = secondUv;
                 uvs[(i * 4) + 2] = thirdUv;
                 uvs[(i * 4) + 3] = fourthUv;
-            }
 
-            uint vertexIndex = 0;
-            for (uint i = 0; i < text.Length; i++)
-            {
                 indices[(i * 6) + 0] = vertexIndex;
                 indices[(i * 6) + 1] = vertexIndex + 1;
                 indices[(i * 6) + 2] = vertexIndex + 2;
@@ -293,20 +255,6 @@ namespace Rendering.Systems
                 indices[(i * 6) + 4] = vertexIndex + 3;
                 indices[(i * 6) + 5] = vertexIndex;
                 vertexIndex += 4;
-            }
-
-            //align
-            Vector2 alignOffset = new(maxPosition.X * alignment.X, maxPosition.Y * alignment.Y);
-            for (uint i = 0; i < text.Length; i++)
-            {
-                ref MeshVertexPosition first = ref positions[(i * 4) + 0];
-                ref MeshVertexPosition second = ref positions[(i * 4) + 1];
-                ref MeshVertexPosition third = ref positions[(i * 4) + 2];
-                ref MeshVertexPosition fourth = ref positions[(i * 4) + 3];
-                first = new(first.value.X - alignOffset.X, first.value.Y - alignOffset.Y, first.value.Z);
-                second = new(second.value.X - alignOffset.X, second.value.Y - alignOffset.Y, second.value.Z);
-                third = new(third.value.X - alignOffset.X, third.value.Y - alignOffset.Y, third.value.Z);
-                fourth = new(fourth.value.X - alignOffset.X, fourth.value.Y - alignOffset.Y, fourth.value.Z);
             }
 
             operation.ResizeArray<MeshVertexPosition>(positions.Length);
@@ -317,14 +265,14 @@ namespace Rendering.Systems
             operation.SetArrayElements(0, indices.AsSpan());
         }
 
-        private unsafe CompiledFont GetOrCompileFont(Entity fontEntity, uint glyphCount, int pixelSize)
+        private unsafe CompiledFont GetOrCompileFont(Entity fontEntity, uint glyphCount, uint pixelSize)
         {
             if (!compiledFonts.TryGetValue(fontEntity.value, out CompiledFont compiledFont))
             {
                 //because we know its a Font, we know it was loaded from bytes before so it must have that list
                 USpan<byte> bytes = fontEntity.GetArray<byte>();
                 Face face = freeType.Load(bytes.pointer, bytes.Length);
-                face.SetPixelSize((uint)pixelSize, (uint)pixelSize);
+                face.SetPixelSize(pixelSize, pixelSize);
 
                 //generate a new texture atlas to be reused
                 using UnmanagedList<AtlasTexture.InputSprite> inputSprites = new();
