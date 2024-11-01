7cce542b361c71dc2259c6ba4c5b08d547b3ba3a
Phill <philldamaskin@gmail.com>
2024-11-01 15:26:56 -0400

    update nunit package and changes after simulation

--- source/TextRasterizationSystem.cs
+++ source/TextRasterizationSystem.cs
@@ -26,7 +26,7 @@ namespace Rendering.Systems
         private readonly List<Operation> operations;
 
         readonly unsafe InitializeFunction ISystem.Initialize => new(&Initialize);
-        readonly unsafe IterateFunction ISystem.Update => new(&Update);
+        readonly unsafe IterateFunction ISystem.Iterate => new(&Update);
         readonly unsafe FinalizeFunction ISystem.Finalize => new(&Finalize);
 
         [UnmanagedCallersOnly]
