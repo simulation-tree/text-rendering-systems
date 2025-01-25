3e15023ca85947a29dbd0768b1771f6b0b01803d
Phill <philldamaskin@gmail.com>
2025-01-24 22:17:38 -0500

    simplify tests initialization

--- tests/TextMeshTests.cs
+++ tests/TextMeshTests.cs
@@ -1,91 +1,15 @@
 ﻿using Data;
-using Data.Components;
-using Data.Systems;
 using Fonts;
-using Fonts.Components;
-using Fonts.Systems;
 using Meshes;
-using Meshes.Components;
 using Rendering;
-using Rendering.Components;
-using Simulation.Tests;
 using System.Threading;
 using System.Threading.Tasks;
-using Textures;
-using Textures.Components;
-using Types;
 using Worlds;
 
 namespace TextRendering.Systems.Tests
 {
-    public class TextMeshTests : SimulationTests
+    public class TextMeshTests : TextRenderingSystemsTests
     {
-        static TextMeshTests()
-        {
-            TypeLayout.Register<IsMesh>();
-            TypeLayout.Register<IsTextMesh>();
-            TypeLayout.Register<IsTextMeshRequest>();
-            TypeLayout.Register<IsTextRenderer>();
-            TypeLayout.Register<IsFont>();
-            TypeLayout.Register<IsFontRequest>();
-            TypeLayout.Register<IsGlyph>();
-            TypeLayout.Register<IsTexture>();
-            TypeLayout.Register<IsTextureRequest>();
-            TypeLayout.Register<IsDataRequest>();
-            TypeLayout.Register<IsDataSource>();
-            TypeLayout.Register<IsData>();
-            TypeLayout.Register<FontMetrics>();
-            TypeLayout.Register<FontName>();
-            TypeLayout.Register<BinaryData>();
-            TypeLayout.Register<AtlasSprite>();
-            TypeLayout.Register<Pixel>();
-            TypeLayout.Register<Kerning>();
-            TypeLayout.Register<FontGlyph>();
-            TypeLayout.Register<TextCharacter>();
-            TypeLayout.Register<MeshVertexPosition>();
-            TypeLayout.Register<MeshVertexNormal>();
-            TypeLayout.Register<MeshVertexUV>();
-            TypeLayout.Register<MeshVertexColor>();
-            TypeLayout.Register<MeshVertexTangent>();
-            TypeLayout.Register<MeshVertexBiTangent>();
-            TypeLayout.Register<MeshVertexIndex>();
-        }
-
-        protected override void SetUp()
-        {
-            base.SetUp();
-            world.Schema.RegisterComponent<IsMesh>();
-            world.Schema.RegisterComponent<IsTextMesh>();
-            world.Schema.RegisterComponent<IsTextMeshRequest>();
-            world.Schema.RegisterComponent<IsTextRenderer>();
-            world.Schema.RegisterComponent<IsFont>();
-            world.Schema.RegisterComponent<IsFontRequest>();
-            world.Schema.RegisterComponent<IsGlyph>();
-            world.Schema.RegisterComponent<IsTexture>();
-            world.Schema.RegisterComponent<IsTextureRequest>();
-            world.Schema.RegisterComponent<IsDataRequest>();
-            world.Schema.RegisterComponent<IsDataSource>();
-            world.Schema.RegisterComponent<IsData>();
-            world.Schema.RegisterComponent<FontMetrics>();
-            world.Schema.RegisterComponent<FontName>();
-            world.Schema.RegisterArrayElement<BinaryData>();
-            world.Schema.RegisterArrayElement<AtlasSprite>();
-            world.Schema.RegisterArrayElement<Pixel>();
-            world.Schema.RegisterArrayElement<Kerning>();
-            world.Schema.RegisterArrayElement<FontGlyph>();
-            world.Schema.RegisterArrayElement<TextCharacter>();
-            world.Schema.RegisterArrayElement<MeshVertexPosition>();
-            world.Schema.RegisterArrayElement<MeshVertexNormal>();
-            world.Schema.RegisterArrayElement<MeshVertexUV>();
-            world.Schema.RegisterArrayElement<MeshVertexColor>();
-            world.Schema.RegisterArrayElement<MeshVertexTangent>();
-            world.Schema.RegisterArrayElement<MeshVertexBiTangent>();
-            world.Schema.RegisterArrayElement<MeshVertexIndex>();
-            simulator.AddSystem<DataImportSystem>();
-            simulator.AddSystem<FontImportSystem>();
-            simulator.AddSystem<TextRasterizationSystem>();
-        }
-
         [Test, CancelAfter(4000)]
         public async Task GenerateTextMesh(CancellationToken cancellation)
         {
---
+++ tests/TextRenderingSystemsTests.cs
@@ -0,0 +1,39 @@
+﻿using Data.Systems;
+using Fonts.Systems;
+using Simulation.Tests;
+using Types;
+using Worlds;
+
+namespace TextRendering.Systems.Tests
+{
+    public abstract class TextRenderingSystemsTests : SimulationTests
+    {
+        static TextRenderingSystemsTests()
+        {
+            TypeRegistry.Load<TextRendering.TypeBank>();
+            TypeRegistry.Load<Data.TypeBank>();
+            TypeRegistry.Load<Fonts.TypeBank>();
+            TypeRegistry.Load<Textures.TypeBank>();
+            TypeRegistry.Load<Meshes.TypeBank>();
+        }
+
+        protected override Schema CreateSchema()
+        {
+            Schema schema = base.CreateSchema();
+            schema.Load<TextRendering.SchemaBank>();
+            schema.Load<Data.SchemaBank>();
+            schema.Load<Fonts.SchemaBank>();
+            schema.Load<Textures.SchemaBank>();
+            schema.Load<Meshes.SchemaBank>();
+            return schema;
+        }
+
+        protected override void SetUp()
+        {
+            base.SetUp();
+            simulator.AddSystem<DataImportSystem>();
+            simulator.AddSystem<FontImportSystem>();
+            simulator.AddSystem<TextRasterizationSystem>();
+        }
+    }
+}
